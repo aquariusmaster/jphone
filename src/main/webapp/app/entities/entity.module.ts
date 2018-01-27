103e09f862561496b397fa1174b30169ad4d3ba8
Andrey Bobrov <verydrybeaver@gmail.com>
2018-01-27 23:40:38 +0200

    add client and manager

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JphoneClientModule } from './client/client.module';
+import { JphoneManagerModule } from './manager/manager.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        JphoneClientModule,
+        JphoneManagerModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
